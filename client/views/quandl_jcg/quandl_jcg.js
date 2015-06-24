f38cde05d1ec791518c5108f4c75410b05048e58
jmunsch <james.a.munsch@gmail.com>
2015-06-24 15:06:42 -0500

    attempting to install the quandl api

---
+++ client/views/quandl_jcg/quandl_jcg.js
@@ -0,0 +1,23 @@
+Template.quandl_jcg.created = function () {
+  //
+};
+
+Template.quandl_jcg.helpers({
+  
+  myHelper: function () {
+    //
+  }
+
+});
+
+Template.quandl_jcg.rendered = function () {
+  //
+};
+
+Template.quandl_jcg.events({
+
+  'click': function(e, instance){
+
+  }
+
+});
