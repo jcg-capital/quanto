4cdabe16b8a828e7ada89fa11fdbfde342adfb60
C S Harding <charliesharding@gmail.com>
2015-06-25 10:12:11 -0500

    end of day commit

---
+++ client/views/landingPage/landingpage.js
@@ -0,0 +1,10 @@
+
+// Here is the positioning data for the node in the landingPage template
+
+Template.landingPage.helpers({
+  nodeSize: "P:0.5; P:0.5; P:0.5",
+  nodeAlign: [0.5, 0.5],
+  nodeMountPoint: [0.5, 0.5],
+  nodeOrigin: [0.5, 0.5],
+  nodeRotation: [.4,.4]
+})
