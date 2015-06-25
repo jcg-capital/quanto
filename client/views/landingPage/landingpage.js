9308c5f0aa2cb24cc6e53a365f54340868762458
C S Harding <charliesharding@gmail.com>
2015-06-25 13:04:33 -0500

    (feat) enabled transparent money tornado that spins when you click it

--- client/views/landingPage/landingpage.js
+++ client/views/landingPage/landingpage.js
@@ -2,13 +2,9 @@ Template.landingPage.created = function () {
   //
 };
 
-Template.landingPage.helpers({
-  
-  myHelper: function () {
-    //
-  }
+// Here is the positioning data for the node in the landingPage template
 
-});
+
 
 Template.landingPage.rendered = function () {
   //
@@ -55,3 +51,33 @@ Template.landingPage.events({
 
 });
 
+var ROTATIONS = [
+  [ Math.PI/8 , Math.PI/4 ],
+  [ -1 * Math.PI/10 , 7 * Math.PI + Math.PI/4]
+]
+
+Session.setDefault('rotation', 0);
+
+Template.landingPage.helpers({
+  reactiveRotation: function() {
+    return {
+      value: ROTATIONS[Session.get('rotation')],
+      // value1: ROTATIONS[0],
+      // value2: ROTATIONS[1],
+      transition: { duration: 3500, curve: 'inOutSine' }
+    }
+  },
+  nodeSize: "P:0.5; P:0.5; P:0.5",
+  nodeAlign: [0.5, 0.5],
+  nodeMountPoint: [0.5, 0.5],
+  nodeOrigin: [0.55, 0.5]
+})
+
+Template.inner.events({
+  'click': function(event, tpl) {
+    console.log("CLICKED")
+    var current = Session.get('rotation');
+    // logic to alternate between first and second position in ROTATIONS array
+    Session.set('rotation', (current+1) % 2 );
+  }
+})
