583ff807fb6b325ed1bd5e58d60bbc6ed46a9316
jmunsch <james.a.munsch@gmail.com>
2015-06-25 14:13:27 -0500

    Added quandl search    Changes to be committed:   	modified:   .meteor/packages   	modified:   .meteor/versions   	modified:   client/views/common/header.html   	new file:   client/views/common/header.html.orig   	modified:   client/views/landingPage/landingpage.html   	modified:   client/views/landingPage/landingpage.js   	new file:   public/logo.png   	modified:   server/router.js

--- client/views/landingPage/landingpage.js
+++ client/views/landingPage/landingpage.js
@@ -16,19 +16,42 @@ Template.landingPage.rendered = function () {
 
 Template.landingPage.events({
 
-  'submit': function(e, instance){
+  'keyup': function(e, instance){
     e.preventDefault()
-
-    var tickerSymbol = e.target.searchQuery.value;
-
-    HTTP.call("POST", "quandlquery", query,
+    var queryString = e.currentTarget.value;
+    var query = {
+                  data: {
+                    query: queryString,
+                    formatObject : { format: "json" } // csv, xml, json
+                  } 
+                };
+    HTTP.call("POST", "quandlsearch", query,
       function (error, result) {
         if (!error) {
-          console.log('WE GOT SOMETHiNG HEREsfsfsafee3',result);
-            var r = JSON.parse(result.content);
+          console.log('SEARCH RESULTS',result);
+          var r = JSON.parse(result.content);
+          $('#search-results').html('');
+          r.docs.forEach(function(c,i,a){
+            var $result = $('<div style="background-color: #999;color: #EEE;margin: 3px;">')
+            var $a = $('<div>').text(c.description);
+            var $b = $('<div>').text(c.frequency);
+            var $c = $('<div>').text(c.name);
+            var $d = $('<div>').text(c.source_code);
+            var $e = $('<div>').text(c.source_name);
+            var $f = $('<div>').text(c.urlize_name);
+            $result.append([$a,$b,$c,$d,$e,$f])
+            $('#search-results').append($result);
+          })
+           // urlize_name: "C-Citigroup-Inc-Common-Stock"
+           // description: "Exchange : . Key Statistics"
+           // frequency: "daily"
+           // name: "C: Citigroup Inc. Common Stock -"
+           // source_code: "YAHOO"
+           // source_name: "YFinance"
         }
       }
     );
   }
 
 });
+
--- server/router.js
+++ server/router.js
@@ -10,7 +10,61 @@ Server-side Router.
 // Router.use(Iron.Router.bodyParser.json({
 //     limit: "100mb"
 //   }));
+
+
+
 Router.map(function() {
+    this.route('quandlmetadata', {
+        where: 'server',
+        action: function() {
+          // GET, POST, PUT, DELETE
+          var requestMethod = this.request.method;
+          // Data from a POST request
+          var requestData = this.request.body;
+          var response = this.response
+          // Setup Quandl api connection
+          var Quandl = Meteor.npmRequire('quandl');
+          var quandl = new Quandl({
+              auth_token: 'vtyjDff63Y15eQuxnSGK',
+              api_version: 1,
+          });
+          //*****************************************//
+          // FIXME: pass parameters along? possibly useful for 
+          // chart formatting?
+          //*****************************************//
+          quandl.metadata("ZILLOW", "ZIP_ALLHOMES_15235", function(err, data){
+            if(err){
+              console.log('quandlStock : ERROR',err);
+            }
+            response.end(data);
+          });
+        }
+    });
+
+
+    this.route('quandlsearch', {
+        where: 'server',
+        action: function() {
+          // GET, POST, PUT, DELETE
+          var requestMethod = this.request.method;
+          // Data from a POST request
+          var requestData = this.request.body;
+          var response = this.response
+          // Setup Quandl api connection
+          var Quandl = Meteor.npmRequire('quandl');
+          var quandl = new Quandl({
+              auth_token: 'vtyjDff63Y15eQuxnSGK',
+              api_version: 1,
+          });
+          quandl.search(requestData.query, requestData.formatObject, function(err, data){
+            if(err){
+              console.log('quandlStock : ERROR',err);
+            }
+            response.end(data);
+          });
+        }
+    });
+
     this.route('quandlquery', {
         where: 'server',
         action: function() {
