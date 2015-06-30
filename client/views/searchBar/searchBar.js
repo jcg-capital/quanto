08ab90daf05fbfa5a2a78e2deafe62594aadf0e4
jmunsch <james.a.munsch@gmail.com>
2015-06-30 12:54:49 -0500

    included wokring search bar?

--- client/views/searchBar/searchBar.js
+++ client/views/searchBar/searchBar.js
@@ -13,7 +13,7 @@ Template.searchBar.rendered = function () {
 Template.searchBar.events({
 
   'keyup': function(e, instance){
-    e.preventDefault()
+    e.preventDefault();
     var queryString = e.currentTarget.value;
     var query = {
                   data: {
@@ -28,16 +28,16 @@ Template.searchBar.events({
           var r = JSON.parse(result.content);
           $('#search-results').html('');
           r.docs.forEach(function(c,i,a){
-            var $result = $('<div style="background-color: #999;color: #EEE;margin: 3px;">')
+            var $result = $('<div style="background-color: #999;color: #EEE;margin: 3px;">');
             var $a = $('<div>').text(c.description);
             var $b = $('<div>').text(c.frequency);
             var $c = $('<div>').text(c.name);
             var $d = $('<div>').text(c.source_code);
             var $e = $('<div>').text(c.source_name);
             var $f = $('<div>').text(c.urlize_name);
-            $result.append([$a,$b,$c,$d,$e,$f])
+            $result.append([$a,$b,$c,$d,$e,$f]);
             $('#search-results').append($result);
-          })
+          });
            // urlize_name: "C-Citigroup-Inc-Common-Stock"
            // description: "Exchange : . Key Statistics"
            // frequency: "daily"
@@ -51,33 +51,33 @@ Template.searchBar.events({
 
 });
 
-var ROTATIONS = [
-  [ Math.PI/8 , Math.PI/4 ],
-  [ -1 * Math.PI/10 , 7 * Math.PI + Math.PI/4]
-];
+// var ROTATIONS = [
+//   [ Math.PI/8 , Math.PI/4 ],
+//   [ -1 * Math.PI/10 , 7 * Math.PI + Math.PI/4]
+// ];
 
-Session.setDefault('rotation', 0);
+// Session.setDefault('rotation', 0);
 
-Template.searchBar.helpers({
-  reactiveRotation: function() {
-    return {
-      value: ROTATIONS[Session.get('rotation')],
-      // value1: ROTATIONS[0],
-      // value2: ROTATIONS[1],
-      transition: { duration: 3500, curve: 'inOutElastic' }
-    };
-  },
-  nodeSize: "P:0.5; P:0.5; P:0.5",
-  nodeAlign: [0.5, 0.5],
-  nodeMountPoint: [0.5, 0.5],
-  nodeOrigin: [0.55, 0.5]
-});
+// Template.searchBar.helpers({
+//   reactiveRotation: function() {
+//     return {
+//       value: ROTATIONS[Session.get('rotation')],
+//       // value1: ROTATIONS[0],
+//       // value2: ROTATIONS[1],
+//       transition: { duration: 3500, curve: 'inOutElastic' }
+//     };
+//   },
+//   nodeSize: "P:0.5; P:0.5; P:0.5",
+//   nodeAlign: [0.5, 0.5],
+//   nodeMountPoint: [0.5, 0.5],
+//   nodeOrigin: [0.55, 0.5]
+// });
 
-Template.inner.events({
-  'click': function(event, tpl) {
-    console.log("CLICKED")
-    var current = Session.get('rotation');
-    // logic to alternate between first and second position in ROTATIONS array
-    Session.set('rotation', (current+1) % 2 );
-  }
-})
+// Template.inner.events({
+//   'click': function(event, tpl) {
+//     console.log("CLICKED")
+//     var current = Session.get('rotation');
+//     // logic to alternate between first and second position in ROTATIONS array
+//     Session.set('rotation', (current+1) % 2 );
+//   }
+// })
