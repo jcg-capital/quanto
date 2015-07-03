c0e3dd11fae2da579c74fd87a4ce73d86be8e8d7
Garrett <gfitzgerald123@gmail.com>
2015-07-03 09:40:57 -0500

    built separate ifs in chart object

--- client/views/charts/charts.js
+++ client/views/charts/charts.js
@@ -79,108 +79,26 @@ setInterval(function() {
   Template.charts.rendered();
 }, 4000);
 
+
 // Streamy.on('hello', function(){
 //   console.log('streamy2');
 //   live = true;
 //   Template.charts.rendered();
 // });
 
+
+
 Template.charts.rendered = function() { 
   var dataObject = Session.get('dataStore');
   // ticker of initial data to query
   var tickerSymbol = 'GOOG';
 
+  // If no data object, load default chart
+  // If live, load symbol and stream
+  // If not live and data object, load dataobject
 
-  if (live) {
-        $(function () {
-
-          //need to pass in if symbol is currently choosen
-
-        Highcharts.setOptions({
-            global : {
-                useUTC : false
-            }
-        });
-
-        // Create the chart
-        $('#container').highcharts('StockChart', {
-            chart : {
-                events : {
-                    load : function () {
-                          //  Streamy.on('hello', function(d) {
-                          //   console.log('data');
-                          //   var data = JSON.parse(d.data); // Will print 'world!'
-                            
-                          //   if (data.trade) {
-                          //     console.log('trade occured');
-                          //      var trade = data.trade;
-                          //      var lastPrice = data.trade.last;
-                          //      var volume = data.trade.cvol;
-                          //      var timestamp = data.trade.timestamp;
-
-                          //      var tradeData = {
-                          //           'lastPrice': lastPrice,
-                          //           'timestamp': timestamp
-                          //      };
-
-                          //      Session.set('tradeData', tradeData);
-                          //      console.log('this is tradeData session:', Session.get('tradeData'));
-                                
-                          //   }
-                           
-                          //     if (data.quote) {
-                          //     console.log('quote occured');
-                          //        var quote = data.quote;
-                          //        var bid = data.quote.bid;
-                          //        var ask = data.quote.ask;
-                          //        var timestamp1 = data.quote.timestamp;
-
-                          //     }
-                          // });
-
-                          //   var series = this.series[0];
-                          //   series.addPoint([timestamp, lastPrice], true, true);
-
-                       
-                     
-                    }
-                }
-            },
-
-            rangeSelector: {
-                buttons: [{
-                    count: 1,
-                    type: 'minute',
-                    text: '1M'
-                }, {
-                    count: 5,
-                    type: 'minute',
-                    text: '5M'
-                }, {
-                    type: 'all',
-                    text: 'All'
-                }],
-                inputEnabled: false,
-                selected: 0
-            },
-
-            title : {
-                text : 'Live random data'
-            },
-
-            exporting: {
-                enabled: false
-            },
-
-            series : [{
-                name : 'Random data',
-                data : []
-                }] 
-              });
-            });
-        }
-
-  if (!dataObject) {    
+ 
+  if (!dataObject && !live) {    
     makeCallRequest(tickerSymbol, function(){
       dataObject = Session.get('dataStore');
       var data = dataObject.data;
@@ -287,112 +205,204 @@ Template.charts.rendered = function() {
       // create the chart
       $('div#container-area').highcharts('StockChart', options);
     }); 
+  } 
+  else if (live) {
+        $(function () {
+
+          //need to pass in if symbol is currently choosen
+
+        Highcharts.setOptions({
+            global : {
+                useUTC : false
+            }
+        });
+
+        // Create the chart
+        $('#container').highcharts('StockChart', {
+            chart : {
+                events : {
+                    load : function () {
+                          //  Streamy.on('hello', function(d) {
+                          //   console.log('data');
+                          //   var data = JSON.parse(d.data); // Will print 'world!'
+                            
+                          //   if (data.trade) {
+                          //     console.log('trade occured');
+                          //      var trade = data.trade;
+                          //      var lastPrice = data.trade.last;
+                          //      var volume = data.trade.cvol;
+                          //      var timestamp = data.trade.timestamp;
+
+                          //      var tradeData = {
+                          //           'lastPrice': lastPrice,
+                          //           'timestamp': timestamp
+                          //      };
+
+                          //      Session.set('tradeData', tradeData);
+                          //      console.log('this is tradeData session:', Session.get('tradeData'));
+                                
+                          //   }
+                           
+                          //     if (data.quote) {
+                          //     console.log('quote occured');
+                          //        var quote = data.quote;
+                          //        var bid = data.quote.bid;
+                          //        var ask = data.quote.ask;
+                          //        var timestamp1 = data.quote.timestamp;
+
+                          //     }
+                          // });
+
+                          //   var series = this.series[0];
+                          //   series.addPoint([timestamp, lastPrice], true, true);
+
+                       
+                     
+                    }
+                }
+            },
+
+            rangeSelector: {
+                buttons: [{
+                    count: 1,
+                    type: 'minute',
+                    text: '1M'
+                }, {
+                    count: 5,
+                    type: 'minute',
+                    text: '5M'
+                }, {
+                    type: 'all',
+                    text: 'All'
+                }],
+                inputEnabled: false,
+                selected: 0
+            },
+
+            title : {
+                text : 'Live random data'
+            },
+
+            exporting: {
+                enabled: false
+            },
+
+            series : [{
+                name : 'Random data',
+                data : []
+                }] 
+              });
+            });
+        }
+
+  else {
+
+        var data = dataObject.data;
+        
+        var columnNames = dataObject.column_names;
+        var ohlc = [];
+        var volume = [];
+        var dataLength = data.length;
+          // set the allowed units for data grouping
+        var groupingUnits = [[
+            'millisecond', // unit name
+            [1, 2, 5, 10, 20, 25, 50, 100, 200, 500] // allowed multiples
+          ], [
+            'second',
+            [1, 2, 5, 10, 15, 30]
+          ], [
+            'minute',
+            [1, 2, 5, 10, 15, 30]
+          ], [
+            'hour',
+            [1, 2, 3, 4, 6, 8, 12]
+          ], [
+            'day',
+            [1]
+          ], [
+            'week',
+            [1]
+          ], [
+            'month',
+            [1, 3, 6]
+          ], [
+            'year',
+            null
+          ]];
+
+        i = 0;
+
+        for (i; i < dataLength; i += 1) {
+          // start of data in milliseconds
+          var newDate = new Date(data[i][0]); // the date
+          var dateInMil = newDate.getTime();
+          ohlc.push([
+            dateInMil,
+            data[i][1], // open
+            data[i][2], // high
+            data[i][3], // low
+            data[i][4] // close
+          ]);
+          volume.push([
+            dateInMil, // the date
+            data[i][5] // the volume
+          ]);
+        }
+
+        // frequency of data - daily, monthly, 
+        var frequency = dataObject.frequency;
+        if (frequency === 'daily') {
+        }
+
+        var options = {
+          rangeSelector: {
+            allButtonsEnabled: true,
+            selected: 2
+          },
+          title: {
+            text: dataObject.name
+          },
+          yAxis: [{
+            labels: {
+              align: 'right',
+              x: -3
+            },
+            title: {
+              text: 'OHLC'
+            },
+            height: '60%',
+            lineWidth: 2
+          }, { 
+            labels: {
+              align: 'right',
+              x: -3
+            },
+            title: {
+              text: 'Volume'
+            },
+            top: '65%',
+            height: '35%',
+            offset: 0,
+            lineWidth: 2
+          }],
+          series: [{
+            type: 'candlestick',
+            data: ohlc,
+            dataGrouping: {
+              units: groupingUnits
+            }
+          }, {
+            type: 'column',
+            name: 'Volume',
+            data: volume,
+            yAxis: 1,
+          }]
+        };
+
+        // create the chart
+        $('div#container-area').highcharts('StockChart', options);
   }
 
-  var data = dataObject.data;
-  
-  var columnNames = dataObject.column_names;
-  var ohlc = [];
-  var volume = [];
-  var dataLength = data.length;
-    // set the allowed units for data grouping
-  var groupingUnits = [[
-      'millisecond', // unit name
-      [1, 2, 5, 10, 20, 25, 50, 100, 200, 500] // allowed multiples
-    ], [
-      'second',
-      [1, 2, 5, 10, 15, 30]
-    ], [
-      'minute',
-      [1, 2, 5, 10, 15, 30]
-    ], [
-      'hour',
-      [1, 2, 3, 4, 6, 8, 12]
-    ], [
-      'day',
-      [1]
-    ], [
-      'week',
-      [1]
-    ], [
-      'month',
-      [1, 3, 6]
-    ], [
-      'year',
-      null
-    ]];
-
-  i = 0;
-
-  for (i; i < dataLength; i += 1) {
-    // start of data in milliseconds
-    var newDate = new Date(data[i][0]); // the date
-    var dateInMil = newDate.getTime();
-    ohlc.push([
-      dateInMil,
-      data[i][1], // open
-      data[i][2], // high
-      data[i][3], // low
-      data[i][4] // close
-    ]);
-    volume.push([
-      dateInMil, // the date
-      data[i][5] // the volume
-    ]);
-  }
-
-  // frequency of data - daily, monthly, 
-  var frequency = dataObject.frequency;
-  if (frequency === 'daily') {
-  }
-
-  var options = {
-    rangeSelector: {
-      allButtonsEnabled: true,
-      selected: 2
-    },
-    title: {
-      text: dataObject.name
-    },
-    yAxis: [{
-      labels: {
-        align: 'right',
-        x: -3
-      },
-      title: {
-        text: 'OHLC'
-      },
-      height: '60%',
-      lineWidth: 2
-    }, { 
-      labels: {
-        align: 'right',
-        x: -3
-      },
-      title: {
-        text: 'Volume'
-      },
-      top: '65%',
-      height: '35%',
-      offset: 0,
-      lineWidth: 2
-    }],
-    series: [{
-      type: 'candlestick',
-      data: ohlc,
-      dataGrouping: {
-        units: groupingUnits
-      }
-    }, {
-      type: 'column',
-      name: 'Volume',
-      data: volume,
-      yAxis: 1,
-    }]
-  };
-
-  // create the chart
-  $('div#container-area').highcharts('StockChart', options);
 };
 
 makeCallRequest = function(ticker, cb) {
