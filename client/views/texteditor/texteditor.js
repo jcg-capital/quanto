086dc9c84aa908eee1462e1ef34c837e13c23b96
Garrett <gfitzgerald123@gmail.com>
2015-07-11 11:57:46 -0500

    added highcharts to splash page

--- client/views/texteditor/texteditor.js
+++ client/views/texteditor/texteditor.js
@@ -18,27 +18,27 @@ Template.textEditor.events({
 
   'click #algo-tab': function(e, instance){
     e.preventDefault();
-    console.log(e)
-    Template.textEditor.rendered()
+    console.log(e);
+    Template.textEditor.rendered();
   },
 
   'click #graph-data': function(e, instance){
     e.preventDefault();
     console.log(e);
-    var t = AceEditor.instance('#text-editor-id')
-    var code = t.getSession().getValue().replace(/\'/g, '\"')
+    var t = AceEditor.instance('#text-editor-id');
+    var code = t.getSession().getValue().replace(/\'/g, '\"');
     eval(code);
     // eval()
     // $('#chart-container').addSeries(Session.get('currentDataStore'))
   },
   'click #save-algo': function(e, instance){
     e.preventDefault();
-    console.log(e)
-    var title = prompt('Name of new algo')
-    var t = AceEditor.instance('#text-editor-id')
-    var code = t.getSession().getValue().replace(/\'/g, '\"')
-    Algorithms.insert({title: title, body: code })
-    alert('Saved code: '+code)
+    console.log(e);
+    var title = prompt('Name of new algo');
+    var t = AceEditor.instance('#text-editor-id');
+    var code = t.getSession().getValue().replace(/\'/g, '\"');
+    Algorithms.insert({title: title, body: code });
+    alert('Saved code: '+code);
   }
 });
  
