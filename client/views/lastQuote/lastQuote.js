69b379215bb87d27afcebda398dca775005aa190
Garrett <gfitzgerald123@gmail.com>
2015-07-11 09:24:55 -0500

    bid/ask on front page

--- client/views/lastQuote/lastQuote.js
+++ client/views/lastQuote/lastQuote.js
@@ -5,6 +5,16 @@
                             var ask;
 
 
+
+                   setInterval(function () {
+                          var x = (new Date()).getTime(), // current time
+                          y = Math.round(Math.random() * 20);
+                              Session.set('lastAsk', y);
+                          z = y+.50
+
+                      }, 1000);
+
+
    Streamy.on('hello', function(d) {
                             console.log('streamy triggered');
                             var data = JSON.parse(d.data); // Will print 'world!'
